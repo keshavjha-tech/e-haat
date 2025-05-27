95a984e9c4632e5261c4786f59a5e7e174940a0a
keshavjha205 <keshavjha1081@gmail.com>
2025-05-27 09:48:15 +0530

    Order Model

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -4,6 +4,46 @@ const orderSchema = new mongoose.Schema({
     userId : {
         type : mongoose.Schema.ObjectId,
         ref : 'User'
+    },
+    orderId : {
+        type : String,
+        required : [true, "Provide OrderId"],
+        unique : true
+    },
+    productId : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'product',
+    },
+    product_details : {
+        name : String,
+        image : Array       
+    },
+    payment_id : {
+        type : String,
+        default : ""
+    },
+    payment_status : {
+        type : String,
+        default : ""
+    },
+    delivery_address : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'address'
+    },
+    delivery_status : {
+        type :String
+    },
+    subTotalAmt : {
+        type : Number,
+        default : 0
+    },
+    totalAmt : {
+        type : Number,
+        default : 0
+    },
+    invoice_receipt : {
+        type : String,
+        default : ""
     }
 
 }, {timestamps : true})

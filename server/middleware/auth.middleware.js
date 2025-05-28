9ebca2266f5619382f9a66d511bb74f79a577601
keshavjha205 <keshavjha1081@gmail.com>
2025-05-28 11:06:26 +0530

    middleware added for jwt verification

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,36 @@
+
+import jwt from "jsonwebtoken"
+
+export const verifyJWT =async(req, res, next) => {
+try {
+    const token = req.cookies.accessToken || req?.header?.authorization?.split(" ")[1]    // ["Bearer", "token"]
+    // console.log(" Access Token Received:", token);
+
+    if(!token){
+        return res.status(401).json({
+            message : "Unauthorized access",
+        })
+    }
+
+    const decode =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+
+    if(!decode){
+        return res.status(401).json({
+            message : "Unauthorized access",
+            error : true,
+            success : false
+        })
+    }
+
+    req.userId = decode.id
+
+    next();
+
+} catch (error) {
+    return res.status(500).json({
+        message : error.message || error,
+        error : true,
+        success : false
+    })
+}
+}

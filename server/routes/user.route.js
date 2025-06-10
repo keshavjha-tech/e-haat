70ae978c69b0e5b3bd949e8885af70eaef97b0a3
keshavjha205 <keshavjha1081@gmail.com>
2025-06-10 20:48:01 +0530

    upload avatar route added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,9 +3,11 @@ import
 {loginController,
     logoutController,
     registerUserController,
+    uploadAvatar,
     verifyEmailController} 
 from '../controllers/user.controller.js'
 import { verifyJWT } from '../middleware/auth.middleware.js';
+import upload from '../middleware/multer.middleware.js';
 
 
 const userRouter = Router();
@@ -16,5 +18,6 @@ userRouter.route('/register').post(registerUserController);
 userRouter.route('/verify-email').post(verifyEmailController);
 userRouter.route('/login').post(loginController);
 userRouter.route('/logout').get(verifyJWT,logoutController);
+userRouter.route('/upload-avatar').put(verifyJWT, upload.single('avatar'), uploadAvatar)
 
 export default userRouter

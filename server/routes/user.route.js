752a458e54248440923d90d3f85177c0c272eafe
keshavjha205 <keshavjha1081@gmail.com>
2025-06-11 11:41:36 +0530

    update user and forgot password routes added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,7 +3,9 @@ import
 {loginController,
     logoutController,
     registerUserController,
+    updateUserDetails,
     uploadAvatar,
+    forgotPasswordController,
     verifyEmailController} 
 from '../controllers/user.controller.js'
 import { verifyJWT } from '../middleware/auth.middleware.js';
@@ -19,5 +21,7 @@ userRouter.route('/verify-email').post(verifyEmailController);
 userRouter.route('/login').post(loginController);
 userRouter.route('/logout').get(verifyJWT,logoutController);
 userRouter.route('/upload-avatar').put(verifyJWT, upload.single('avatar'), uploadAvatar)
+userRouter.route('/update-user').put(verifyJWT, updateUserDetails)
+userRouter.route('/forgot-password').put(forgotPasswordController)
 
 export default userRouter

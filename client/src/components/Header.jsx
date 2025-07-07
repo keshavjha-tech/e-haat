2912a9d4f163c504be87ac318eaa8ac2ac2e13fa
keshavjha205 <keshavjha1081@gmail.com>
2025-07-07 21:02:30 +0530

    added cart, login routes and icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,48 +1,78 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import { Link } from 'react-router-dom'
+import React from "react";
+import SearchBar from "./SearchBar";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
+import { LuCircleUserRound } from "react-icons/lu";
+import useMobile from "../hooks/useMobile";
 
 function Header() {
+  const [isMobile] = useMobile();
+  const location = useLocation();
+  const isSearchPage = location.pathname === "/search";
+  const navigate = useNavigate();
+
+  const redirectToLoginPage = () =>{
+    navigate("/login")
+  }
+
+  // console.log("location", location);
+  // console.log('ismobile', isMobile);
+  // console.log("isSearchPage", isSearchPage);
   return (
-    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 items-center flex  flex-col gap-2'>
-      <div className='container mx-auto flex items-center justify-around px-4 '>
-        {/* {Logo} */}
-        <div className='h-full'>
-          <div className=' h-full flex items-center '>
-            <Link to='/'>
-              <h1 className='text-3xl'>
-                <span>e</span><span>Haat</span>
-              </h1>
-            </Link>
+    <header className="h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 items-center flex  flex-col gap-2 justify-center">
+      {!(isSearchPage && isMobile) && (
+        <div className="container mx-auto flex items-center justify-around px-4 ">
+          {/* {Logo} */}
+          <div className="h-full">
+            <div className=" h-full flex items-center ">
+              <Link to="/">
+                <h1 className="text-3xl">
+                  <span>e</span>
+                  <span>Haat</span>
+                </h1>
+              </Link>
+            </div>
           </div>
-        </div>
 
-        {/* {Search} */}
+          {/* {Search} */}
 
-        <div className='hidden lg:block'>
-          <SearchBar />
-        </div>
+          <div className="hidden lg:block">
+            <SearchBar />
+          </div>
 
-        {/* {login and cart} */}
+          {/* {login and cart} */}
 
-        <div >
-          <button className='lg:hidden'>
+          <div>
+            {/* For mobile version  */}
+            <button className="lg:hidden">
               <FaUserCircle size={24} />
-          </button>
+            </button>
 
-          <div className='hidden lg:block'>
-                Login and cart
+            {/* For Desktop */}
+
+            <div className="hidden lg:flex items-center gap-7">
+              <button onClick={redirectToLoginPage} className="flex gap-1 px-2 "> <LuCircleUserRound className="size-6 "/>Login</button>
+
+              <button className="flex items-center gap-2">
+                  {/* cart icon */}
+                <div>
+                  <BsCart3 size={24}/>
+                </div>
+                <div>
+                  <p>cart</p>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
+      )}
 
-      </div>
-
-      <div className='container mx-auto px-2 sm:hidden items-center'>
+      <div className="container mx-auto px-2 lg:hidden items-center">
         <SearchBar />
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;

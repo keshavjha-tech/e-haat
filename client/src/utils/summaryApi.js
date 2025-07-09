de742b964fd8136f5f1a8c11236f48cdcaf2c74c
keshavjha205 <keshavjha1081@gmail.com>
2025-07-09 13:01:02 +0530

    minor change

--- client/src/utils/summaryApi.js
+++ client/src/utils/summaryApi.js
@@ -1,5 +1,7 @@
-export const API_ENDPOINT="http://localhost:8080"
+export const baseURL="http://localhost:8080"
 
+
+//endpoints
 const summaryApi = {
     register : {
         url : '/api/user/register',

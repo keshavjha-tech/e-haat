6c9f42e840f831da13e9a73c4058a2e9d468a0d0
keshavjha205 <keshavjha1081@gmail.com>
2025-07-09 11:12:45 +0530

    endpoint created

---
+++ client/src/utils/summaryApi.js
@@ -0,0 +1,10 @@
+export const API_ENDPOINT="http://localhost:8080"
+
+const summaryApi = {
+    register : {
+        url : '/api/user/register',
+        method : 'post'
+    }
+}
+
+export default summaryApi
